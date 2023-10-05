0aea1f7a8e9af197e793733c152785845173859b
Guilherme Lima Desoler <guilherme.desoler@gmail.com>
2023-10-05 02:53:38 -0300

    fix letter

--- lib/components/letter.dart
+++ lib/components/letter.dart
@@ -8,7 +8,7 @@ class Letter extends StatelessWidget {
   @override
   Widget build(Object context) => Container(
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-        constraints: const BoxConstraints(maxWidth: 480),
+        constraints: const BoxConstraints(maxWidth: 340),
         child: SingleChildScrollView(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
